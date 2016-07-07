5d086d315b310ae0d6302bea112b07c6b40314c5
Rachel Hutchison <Rachel.hutchison@i.tescobank.com>
2016-07-07 22:13:51 +0100

    pulling cake holder fragment into own class

---
+++ app/src/main/java/com/waracle/androidtest/CakeHolderFragment.java
@@ -0,0 +1,119 @@
+package com.waracle.androidtest;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Fragment is responsible for loading in some JSON and
+ * then displaying a list of cakes with images.
+ * Fix any crashes
+ * Improve any performance issues
+ * Use good coding practices to make code more secure
+ */
+public class CakeHolderFragment extends Fragment {
+
+    private static final String TAG = CakeHolderFragment.class.getSimpleName();
+    private static String JSON_URL = "https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/" +
+            "raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json";
+
+    private ListView mListView;
+
+    public CakeHolderFragment() { /**/ }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mListView = (ListView) rootView.findViewById(R.id.list);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Load data from net.
+        new AsyncTask<Void, Void, JSONArray>() {
+            @Override
+            protected JSONArray doInBackground(Void... params) {
+                try {
+                    return loadData();
+                } catch (IOException | JSONException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+                return new JSONArray();
+            }
+
+            @Override
+            protected void onPostExecute(JSONArray jsonArray) {
+                buildCakeList(jsonArray);
+            }
+        }.execute();
+    }
+
+    private void buildCakeList(JSONArray jsonArray) {
+        // Create and set the list adapter.
+        mListView.setAdapter(new CakeListAdapter(getActivity(), jsonArray));
+    }
+
+    private JSONArray loadData() throws IOException, JSONException {
+        URL url = new URL(JSON_URL);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+            // Can you think of a way to improve the performance of loading data
+            // using HTTP headers???
+
+            // Also, Do you trust any utils thrown your way????
+
+            byte[] bytes = StreamUtils.readUnknownFully(in);
+
+            // Read in charset of HTTP content.
+            String charset = parseCharset(urlConnection.getRequestProperty("Content-Type"));
+
+            // Convert byte array to appropriate encoded string.
+            String jsonText = new String(bytes, charset);
+
+            // Read string as JSON.
+            return new JSONArray(jsonText);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    /**
+     * Returns the charset specified in the Content-Type of this header,
+     * or the HTTP default (ISO-8859-1) if none can be found.
+     */
+    public static String parseCharset(String contentType) {
+        if (contentType != null) {
+            String[] params = contentType.split(",");
+            for (int i = 1; i < params.length; i++) {
+                String[] pair = params[i].trim().split("=");
+                if (pair.length == 2) {
+                    if (pair[0].equals("charset")) {
+                        return pair[1];
+                    }
+                }
+            }
+        }
+        return "UTF-8";
+    }
+
+}
