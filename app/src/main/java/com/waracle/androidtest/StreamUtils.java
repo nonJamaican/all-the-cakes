c444200559453c7ac5805083bf69c126b3c6b4f6
Rachel Hutchison <Rachel.hutchison@i.tescobank.com>
2016-07-07 21:59:51 +0100

    chaning input stream covertion to byte array

--- app/src/main/java/com/waracle/androidtest/StreamUtils.java
+++ app/src/main/java/com/waracle/androidtest/StreamUtils.java
@@ -2,37 +2,27 @@ package com.waracle.androidtest;
 
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 
 /**
  * Created by Riad on 20/05/2015.
  */
 public class StreamUtils {
+
     private static final String TAG = StreamUtils.class.getSimpleName();
 
     // Can you see what's wrong with this???
     public static byte[] readUnknownFully(InputStream stream) throws IOException {
         // Read in stream of bytes
-        ArrayList<Byte> data = new ArrayList<>();
-        while (true) {
-            int result = stream.read();
-            if (result == -1) {
-                break;
-            }
-            data.add((byte) result);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int readByte;
+        while ((readByte = stream.read()) != -1) {
+            outputStream.write(readByte);
         }
-
-        // Convert ArrayList<Byte> to byte[]
-        byte[] bytes = new byte[data.size()];
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = data.get(i);
-        }
-
-        // Return the raw byte array.
-        return bytes;
+        return outputStream.toByteArray();
     }
 
     public static void close(Closeable closeable) {
