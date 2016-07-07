2ca782c04d363df1c9d588d24ff22eeac817df50
Rachel Hutchison <Rachel.hutchison@i.tescobank.com>
2016-07-07 22:23:57 +0100

    adding final to static url string

--- app/src/main/java/com/waracle/androidtest/CakeHolderFragment.java
+++ app/src/main/java/com/waracle/androidtest/CakeHolderFragment.java
@@ -28,7 +28,7 @@ import java.net.URL;
 public class CakeHolderFragment extends Fragment {
 
     private static final String TAG = CakeHolderFragment.class.getSimpleName();
-    private static String JSON_URL = "https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/" +
+    private static final String JSON_URL = "https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/" +
             "raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json";
 
     private ListView mListView;
